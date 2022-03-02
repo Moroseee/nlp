9fa01709b31a9995adfa0133637f6d5078a94dd2
Moroseee <90878676+Moroseee@users.noreply.github.com>
2022-03-03 00:02:20 +0800

    Add files via upload

---
+++ pca.py
@@ -0,0 +1,90 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_wine
+from sklearn.pipeline import make_pipeline
+print(__doc__)
+
+# 源代码: Tyler Lanigan <tylerlanigan@gmail.com>
+#        Sebastian Raschka <mail@sebastianraschka.com>
+
+# 执照: BSD 3 clause
+
+RANDOM_STATE = 42
+FIG_SIZE = (10, 7)
+
+
+features, target = load_wine(return_X_y=True)
+
+# 使用30％的测试大小进行训练/测试拆分
+X_train, X_test, y_train, y_test = train_test_split(features, target,
+                                                    test_size=0.30,
+                                                    random_state=RANDOM_STATE)
+
+# 使用管道式GNB和PCA拟合数据并进行预测
+unscaled_clf = make_pipeline(PCA(n_components=2), GaussianNB())
+unscaled_clf.fit(X_train, y_train)
+pred_test = unscaled_clf.predict(X_test)
+
+# 使用管道缩放，GNB和PCA拟合数据并进行预测。
+std_clf = make_pipeline(StandardScaler(), PCA(n_components=2), GaussianNB())
+std_clf.fit(X_train, y_train)
+pred_test_std = std_clf.predict(X_test)
+
+# 在缩放和非缩放数据中显示预测精度
+print('\nPrediction accuracy for the normal test dataset with PCA')
+print('{:.2%}\n'.format(metrics.accuracy_score(y_test, pred_test)))
+
+print('\nPrediction accuracy for the standardized test dataset with PCA')
+print('{:.2%}\n'.format(metrics.accuracy_score(y_test, pred_test_std)))
+
+# 从管道中提取PCA
+pca = unscaled_clf.named_steps['pca']
+pca_std = std_clf.named_steps['pca']
+
+# 展示第一主成分
+print('\nPC 1 without scaling:\n', pca.components_[0])
+print('\nPC 1 with scaling:\n', pca_std.components_[0])
+
+# 可视化使用了缩放和未使用缩放时的PCA
+X_train_transformed = pca.transform(X_train)
+scaler = std_clf.named_steps['standardscaler']
+X_train_std_transformed = pca_std.transform(scaler.transform(X_train))
+
+# 使用PCA可视化标准化和未修改的数据集
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=FIG_SIZE)
+
+
+for l, c, m in zip(range(0, 3), ('blue', 'red', 'green'), ('^', 's', 'o')):
+    ax1.scatter(X_train_transformed[y_train == l, 0],
+                X_train_transformed[y_train == l, 1],
+                color=c,
+                label='class %s' % l,
+                alpha=0.5,
+                marker=m
+                )
+
+for l, c, m in zip(range(0, 3), ('blue', 'red', 'green'), ('^', 's', 'o')):
+    ax2.scatter(X_train_std_transformed[y_train == l, 0],
+                X_train_std_transformed[y_train == l, 1],
+                color=c,
+                label='class %s' % l,
+                alpha=0.5,
+                marker=m
+                )
+
+ax1.set_title('Training dataset after PCA')
+ax2.set_title('Standardized training dataset after PCA')
+
+for ax in (ax1, ax2):
+    ax.set_xlabel('1st principal component')
+    ax.set_ylabel('2nd principal component')
+    ax.legend(loc='upper right')
+    ax.grid()
+
+plt.tight_layout()
+
+plt.show()
